b92fe3da5ba824caa33a76eb50db62b3cf55bbbf
Nils Becker <nils.becker@dreipol.ch>
2021-01-19 10:16:04 +0100

    Setup on device testing

---
+++ Test/dreiAttestTestHost/dreiAttestTestHostApp.swift
@@ -0,0 +1,17 @@
+//
+//  dreiAttestTestHostApp.swift
+//  dreiAttestTestHost
+//
+//  Created by Nils Becker on 19.01.21.
+//
+
+import SwiftUI
+
+@main
+struct dreiAttestTestHostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
