6a388d6816b291a0f13468828ea456434ceea420
Nils Becker <nils.becker@dreipol.ch>
2021-01-20 12:56:37 +0100

    Add SwiftCBOR CocoaPod

--- Test/Pods/Target Support Files/Pods-dreiAttestTests/Pods-dreiAttestTests-frameworks.sh
+++ Test/Pods/Target Support Files/Pods-dreiAttestTests/Pods-dreiAttestTests-frameworks.sh
@@ -177,10 +177,12 @@ code_sign_if_enabled() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Mocker/Mocker.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftCBOR/SwiftCBOR.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Mocker/Mocker.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftCBOR/SwiftCBOR.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
